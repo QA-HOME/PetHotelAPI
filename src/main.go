01398f9dde66c9c399da551afbf0a7e5952645b2
kagan.meric <kagan.meric@enuygun.com>
2023-01-15 03:36:01 +0300

    feat :: index sayfası eklendi.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"v1/src/errors"
+	"v1/src/handlers"
+)
 
 func main() {
-	fmt.Print("PetHotelApi")
+	r := http.NewServeMux()
+	r.HandleFunc("/", handlers.Index)
+
+	log.Default().Print("8081 is started")
+
+	err := http.ListenAndServe(":8081", r)
+	errors.CheckErr(err)
 }
