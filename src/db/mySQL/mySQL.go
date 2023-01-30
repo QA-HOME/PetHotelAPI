6d1c6758d4fdc29c1aa917ec8cc793c86dc8c620
kagan.meric <kagan.meric@enuygun.com>
2023-01-31 01:45:48 +0300

    feat :: project

--- src/db/mySQL/mySQL.go
+++ src/db/mySQL/mySQL.go
@@ -3,13 +3,34 @@ package mySQL
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"v1/src/errors"
 )
 
 const username = "root"
 const password = "12345678"
 
-func main() {
+func Connect() (*gorm.DB, error) {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	_, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "root:12345678@tcp(127.0.0.1:3306)/sys?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	errors.CheckErr(err)
+	if err == nil {
+		log.Default().Print("MYSQL DB CONNECT")
+	}
+	return db, err
+}
+
+func Close(db *gorm.DB) error {
+
+	mySqlDB, err := db.DB()
+	errors.CheckErr(err)
+
+	err = mySqlDB.Close()
+	errors.CheckErr(err)
+
+	if err == nil {
+		log.Default().Print("MYSQL DB DISCONNECT")
+	}
+	return err
 }
