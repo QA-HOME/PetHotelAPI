5045f2a637b36f5a094d709cd1be1508142eaa57
kagan.meric <kagan.meric@enuygun.com>
2023-01-31 01:21:17 +0300

    feat :: project

--- src/db/postgre/postgre.go
+++ src/db/postgre/postgre.go
@@ -3,11 +3,27 @@ package postgre
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 	"v1/src/errors"
 )
 
-func main() {
-	var dsn = "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func ConnectDB() (*gorm.DB, error) {
+
+	var dsn = "host=localhost user=postgres password=postgres dbname=PetHotelAPI port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	errors.CheckErr(err)
+	if err == nil {
+		log.Default().Print("POSTGRE DB IS CONNECTED")
+	}
+	return db, err
+}
+
+func CloseDB(db *gorm.DB) error {
+	sqlDB, _ := db.DB()
+	err := sqlDB.Close()
+	errors.CheckErr(err)
+	if err == nil {
+		log.Default().Print("POSTGRE DB IS DISCONNECTED")
+	}
+	return err
 }
--- src/main.go
+++ src/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"v1/src/db/postgre"
 	"v1/src/errors"
 	"v1/src/handlers"
 )
@@ -14,8 +13,6 @@ func main() {
 	r.HandleFunc("/token", handlers.GetToken)
 
 	log.Default().Print("Server localhost:8081 is started")
-	db := postgre.Connect()
-	postgre.Close(db)
 
 	defer func(addr string, handler http.Handler) {
 		err := http.ListenAndServe(addr, handler)
